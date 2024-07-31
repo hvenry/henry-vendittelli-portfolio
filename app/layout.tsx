0a9c76187fcf387d733c68cd495e86d233239093
hvenry <hvendittelli@gmail.com>
2024-07-31 09:55:40 -0400

    fixed css bug in naming

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
           <div className="h-screen flex justify-center">
             <Navbar navItems={navItems} />
             {/* page content auto-sizing margins and space for nav */}
-            <div className="mx-4 mt-28 h-[calc(100vh-196px)] w-full md:w-2/3 lg:w-1/2">
+            <div className="mx-4 px-4 mt-28 h-[calc(100vh-196px)] w-full md:w-2/3 lg:w-1/2">
               {children}
             </div>
           </div>
