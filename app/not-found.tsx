275285645f9baefddd070e7f865ef8c20491b2f0
hvenry <hvendittelli@gmail.com>
2024-07-31 17:36:54 -0400

    added 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <main className="h-full w-full flex justify-center items-center">
+      <div className="border border-primary basic-glow p-4 flex flex-col gap-4 items-center">
+        <p className="text-3xl">404 - Page not Found.</p>
+        <Link className="text-blue-600 hover:text-blue-300 text-2xl" href="/">
+          back
+        </Link>
+      </div>
+    </main>
+  );
+}
