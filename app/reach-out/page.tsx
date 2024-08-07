310ef0b475bb82397eca3242faa8c20cc0a579d8
hvenry <hvendittelli@gmail.com>
2024-08-07 17:10:24 -0400

    restyled contact page, created new contact component, trimmed css

--- app/reach-out/page.tsx
+++ app/reach-out/page.tsx
@@ -1,40 +1,14 @@
+import ContactCard from "@/components/ContactCard";
+
 export const metadata = {
   title: "Reach Out! - henryvendittelli.com",
-  description:
-    "Henry's contact information + resume.",
+  description: "Henry's contact information + resume.",
 };
 
 export default function Page() {
-  const resume = "/assets/pdfs/HenryVendittelliResume2024.pdf";
-
   return (
-    <main className="h-full flex justify-center items-center">
-      <div className="p-4 w-3/4 sm:w-1/2 lg:w-2/5 border transition duration-300 ease-in-out border-primary basic-glow">
-        <div className="text-3xl font-bold pb-4 flex justify-center">
-          Contact Me
-        </div>
-        <div className="sm:text-2xl text-lg text-primary-1">
-          <div className="flex justify-between">
-            <p>Email:</p>
-            <p>hvendittelli@gmail.com</p>
-          </div>
-          <div className="flex justify-between">
-            <p>Phone:</p>
-            <p>647-926-6820</p>
-          </div>
-          <div className="flex justify-between">
-            <p>Resume:</p>
-            <a
-              className="text-blue-600 hover:text-blue-300"
-              href={resume}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              here
-            </a>
-          </div>
-        </div>
-      </div>
+    <main className="h-full flex flex-col justify-center items-center">
+      <ContactCard />
     </main>
   );
 }
---
+++ components/ContactCard.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useState } from "react";
+
+export default function ContactCard() {
+  const [isHovered, setIsHovered] = useState(false);
+  const resume = "/assets/pdfs/HenryVendittelliResume2024.pdf";
+
+  return (
+    <div
+      className="relative flex justify-center items-center"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div className="p-4 border transition-all duration-300 ease-in-out border-primary basic-glow hover:scale-105">
+        <div className="text-3xl font-bold pb-4 flex justify-center">
+          Contact Me
+        </div>
+        <div className="sm:text-2xl text-lg text-primary-1">
+          <div className="flex justify-between gap-6 sm:gap-12">
+            <p>Email:</p>
+            <p>hvendittelli@gmail.com</p>
+          </div>
+          <div className="flex justify-between">
+            <p>Phone:</p>
+            <p>647-926-6820</p>
+          </div>
+          <div className="flex justify-between">
+            <p>Resume:</p>
+            <a
+              className="text-blue-600 hover:text-blue-300"
+              href={resume}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              here
+            </a>
+          </div>
+        </div>
+      </div>
+      <div
+        className={`absolute bottom-[-60px] text-3xl transition-opacity duration-300 ease-in-out ${
+          isHovered ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        😎
+      </div>
+    </div>
+  );
+}
