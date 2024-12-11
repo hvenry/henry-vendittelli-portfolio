f0697c02a6b019d7b1768ea08f6712f25d818265
hvenry <hvendittelli@gmail.com>
2024-12-11 04:06:46 -0500

    Implemented canvas best practices

--- app/rock/page.tsx
+++ app/rock/page.tsx
@@ -1,48 +1,15 @@
-"use client";
-
 import React from "react";
-import { Canvas } from "@react-three/fiber";
-import { useRef, forwardRef } from 'react';
-import { OrbitControls, useGLTF, Environment, } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
+import RotatingRockCanvas from "@/components/ui/RotatingRock";
 
-// export const metadata = {
-//   title: "Rock - henryvendittelli.com",
-//   description: "3d rock."
-// };
-
-const RockModel = forwardRef((props, ref) => {
-  const { scene } = useGLTF("/models/rock.glb");
-  return <primitive object={scene} ref={ref} {...props} />;
-});
-
-const RotatingRock = () => {
-  const rockRef = useRef<THREE.Object3D>(null);
-
-  useFrame(() => {
-    if (rockRef.current) {
-      // Rotate the object on the Y-axis
-      rockRef.current.rotation.y += 0.005; // Adjust speed by changing this value
-    }
-  });
-
-  return <RockModel ref={rockRef} />;
+export const metadata = {
+  title: "Rock - henryvendittelli.com",
+  description: "3d rock."
 };
 
 export default function Page() {
   return (
     <main className="h-full flex flex-col justify-center items-center">
-      {/* <p> this is my rock </p> */}
-      <Canvas
-        style={{ height: "80%", width: "100%" }}>
-        <Environment preset="studio" />
-        <RotatingRock />
-        <OrbitControls
-          target={[0, 0, 0]} // Set the target the camera orbits around
-          minDistance={1} // Minimum zoom distance
-          maxDistance={1.5} // Maximum zoom distance
-        />
-      </Canvas>
+      <RotatingRockCanvas />
     </main>
   );
 }
---
+++ components/ui/RotatingRock.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import * as THREE from "three";
+import React, { useRef, forwardRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, useGLTF, Environment } from "@react-three/drei";
+
+// RockModel Component
+const RockModel = forwardRef((props, ref) => {
+  const { scene } = useGLTF("/models/rock.glb");
+  return <primitive object={scene} ref={ref} {...props} />;
+});
+
+RockModel.displayName = "RockModel";
+
+// RotatingRock Component
+const RotatingRock = () => {
+  const rockRef = useRef<THREE.Object3D>(null);
+
+  useFrame(() => {
+    if (rockRef.current) {
+      rockRef.current.rotation.y += 0.005; // Adjust rotation speed here
+    }
+  });
+
+  return <RockModel ref={rockRef} />;
+};
+
+// RotatingRockCanvas Component
+const RotatingRockCanvas = () => {
+  return (
+    <Canvas style={{ height: "80%", width: "100%" }}>
+      <Environment preset="studio" />
+      <RotatingRock />
+      <OrbitControls
+        target={[0, 0, 0]}
+        minDistance={1}
+        maxDistance={1.5}
+      />
+    </Canvas>
+  );
+};
+
+export default RotatingRockCanvas;
+
